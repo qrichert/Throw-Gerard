4eff661d977e05de87e07e888aec2a84d7017ecd
Quentin Richert <quentin.richert@gmail.com>
2017-02-10 14:21:29 +0100

    Changed "IS_DEVICE_TEST" to "IS_REAL_DEVICE".

---
+++ www/js/game/Vibration.js
@@ -0,0 +1,15 @@
+/*
+	This file handles phone's vibrations.
+*/
+
+function Vibration() {
+	this.vibrate = function (duration) {
+		
+		if (IS_REAL_DEVICE) {
+			
+		}
+		
+	}
+}
+
+var VIBRATION = new Vibration();
